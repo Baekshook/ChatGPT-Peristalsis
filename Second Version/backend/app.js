0aeada643b021b6d77a5e78f31f0ede0c6bdb3d4
Baekshook <ecgo0106@naver.com>
2023-04-18 14:13:43 +0900

    post 요청 작업 완료

--- Second Version/backend/app.js
+++ Second Version/backend/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -12,11 +13,34 @@ app.use(express.json());
 
 app.post("/chat", async (req, res) => {
   try {
-    console.log(req.headers.authorization?.substring(7));
-    console.log(process.env.SECRET_KEY);
-    console.log(
-      req.headers.authorization?.substring(7) === process.env.SECRET_KEY
+    const { content } = req.body;
+
+    const bearerToken = req.headers.authorization?.substring(7);
+
+    if (bearerToken !== process.env.SECRET_KEY) {
+      return res
+        .status(400)
+        .json({ ok: false, error: "올바른 키를 입력해주세요." });
+    }
+    if (!content) {
+      return res.status(400).json({ ok: false, error: "질문을 입력해주세요." });
+    }
+
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content }],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_KEY}`,
+        },
+      }
     );
+
+    console.log(response.data.choices[0].message.content);
 
     res.send("임시");
   } catch (error) {
