974bb13f33630d0216126bf19ce397c224ec9bc4
Baekshook <ecgo0106@naver.com>
2023-04-18 11:13:43 +0900

    검색중 css styling

--- 1/src/App.jsx
+++ 1/src/App.jsx
@@ -41,7 +41,9 @@ function App() {
     <div className="max-w-screen-md mx-auto min-h-screen flex flex-col justify-start items-center pt-16 px-4">
       <form className="flex w-full" onSubmit={onSubmitChat}>
         <input
-          className="grow border-2 px-2 py-1 border-gray-300 rounded-lg focus:outline-main shadow-lg"
+          className={`grow border-2 px-2 py-1 border-gray-300 rounded-lg focus:outline-main shadow-lg ${
+            isLoading && "bg-gray-200 text-gray-400"
+          }`}
           type="text"
           value={content}
           onChange={(e) => {
@@ -50,9 +52,12 @@ function App() {
           disabled={isLoading}
         />
         <input
-          className="ml-4 px-2 py-1 border-2 border-main text-main rounded-lg shadow-lg"
+          className={`w-24 ml-4 px-2 py-1 border-2 border-main text-main rounded-lg shadow-lg ${
+            isLoading && "bg-main text-gray-200"
+          }`}
           type="submit"
           disabled={isLoading}
+          value={isLoading ? "검색중..." : "검색"}
         />
       </form>
       {result && <div className="mt-16 bg-main p-4 text-gray-50">{result}</div>}
