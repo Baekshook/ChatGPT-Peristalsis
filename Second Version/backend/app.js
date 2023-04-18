e5402313209454b241bf8b766d5b89e3db58fda6
Baekshook <ecgo0106@naver.com>
2023-04-18 11:51:51 +0900

    backend 초기 세팅

---
+++ Second Version/backend/app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port} 🚀`);
+});
